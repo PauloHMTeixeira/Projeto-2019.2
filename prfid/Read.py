234563b0bece9743cf035469eb23c2b90c28bb19
Daví Silva <48020809+davisa6@users.noreply.github.com>
2019-12-02 17:07:57 -0300

    Add files via upload

---
+++ prfid/Read.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+	id, text = reader.read()
+	print(id)
+	print(text)
+
+finally:
+	GPIO.cleanup()
+
+
+if text == 'Cartao 1':
+	print("TDAGS")
